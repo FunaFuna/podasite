e17889bde94d7f6958968353fd2be9492cee6b23
Dmitriy Ryzhiy <dcamozlo@gmail.com>
2021-11-29 17:53:40 +0100

    project pages fixed

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,36 +1,103 @@
 import { useRouter } from "next/dist/client/router";
-import Project from "../../compoments/Project";
-export default function ProjectDynamic({project}){
+import Project from "../../compoments/project";
 
-    const router = useRouter()
-    const {id} = router.query
+export default function ProjectDynamic({ project }) {
+  const router = useRouter();
+  const { id } = router.query;
 
-    return(<><Project data = {project}/>
-    </>)
+  return (
+    <>
+      <Project data={project} />
+    </>
+  );
+}
+const pages = [
+  {
+    id: "0",
+    type: "webpages",
+    header: {
+      headerText: "Высокоэффективные солнечные станции для каждого",
+      smallText: "Разработка сайта для компании 'ION Solar'",
+      img: "../img/ion1.png",
+      imgMob: "../img/ion_mob1.png",
+      button: "Хочу так же",
+    },
+  },
+  {
+    id: "1",
+    type: "webpages",
+    header: {
+      headerText: "Высокоэффективные солнечные станции для каждого",
+      smallText: "Разработка сайта для компании 'ION Solar'",
+      img: "../img/ion1.png",
+      imgMob: "../img/ion_mob1.png",
+      button: "Хочу так же",
+    },
+  },
+  {
+    id: "2",
+    type: "webpages",
+    header: {
+      headerText: "Высокоэффективные солнечные станции для каждого",
+      smallText: "Разработка сайта для компании 'ION Solar'",
+      img: "../img/ion1.png",
+      imgMob: "../img/ion_mob1.png",
+      button: "Хочу так же",
+    },
+  },
+  {
+    id: "5",
+    type: "webpages",
+    header: {
+      headerText: "1111Высокоэффективные солнечные станции для каждого",
+      smallText: "Разработка сайта для компании 'ION Solar'",
+      img: "../img/ion1.png",
+      imgMob: "../img/ion_mob1.png",
+      button: "Хочу так же",
+    },
+  },
+  {
+    id: "4",
+    type: "brandings",
+    header: {
+      headerText: "Брендинг мульти автосервиса в Вроцлаве",
+      smallText: "Реклама в Google Adwords, Facebook, Instagram для ION Solar",
+      img: "../img/ion10.png",
+      imgMob: "../img/brandings_main_mob.png",
+      button: "Смотреть проекты",
+    },
+  },
+  {
+    id: "3",
+    type: "design",
+    header: {
+      headerText: "Брендинг мульти автосервиса в Вроцлаве",
+      smallText: "Разработка сайта для компании 'ION Solar'",
+      img: "../img/ion10.png",
+      imgMob: "../img/brandings_main_mob.png",
+      button: "Смотреть проекты",
+    },
+  },
+];
+export async function getStaticProps({ params }) {
+  const req = pages[params.id];
+  console.log(req);
+  const data = req;
+  return {
+    props: { project: data },
+  };
 }
 
-export async function getStaticProps({params}){
+export async function getStaticPaths() {
+  const req = ["0", "1", "2", "3", "4", "5"];
+  const data = req;
 
-    const req = await fetch(`http://localhost:3000/${params.id}.json`);
-    console.log(req);
-    const data = await req.json();
-    return{
-        props: {project: data},
-    }
+  const paths = data.map((project) => {
+    return { params: { id: project } };
+  });
 
-}
-
-export async function getStaticPaths(){
-    const req = await fetch('http://localhost:3000/projects.json');
-    const data = await req.json();
-
-    const paths = data.map(project => {
-        return {params: {id: project}}
-    })
-
-    return {
-        paths,
-        fallback: false
-    };
-
+  return {
+    paths,
+    fallback: false,
+  };
 }
